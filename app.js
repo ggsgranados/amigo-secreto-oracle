7b3ccac129f373a68d013f8d3fdf2f5125aa4407
ggsgranados <67867979+ggsgranados@users.noreply.github.com>
2025-02-03 13:23:07 -0600

    add function actualizarLista

--- app.js
+++ app.js
@@ -8,24 +8,46 @@ let listadoAmigos = [];
 // Variable que guarda un nombre
 let amigo = "";
 
+// Elemento HTML de la lista
+let lista = "";
+
 // Funcion para agregar un amigo a la lista
 /* Funcionamiento:
     - Busca el elemento por Id y toma su valor
     - Verifica que no este vacio ni nulo
     - Agrega el elemento a la lista
-    - Limpia la caja de texto llamando a otra funcion*/
+    - Limpia la caja de texto llamando a otra funcion
+    - Actualiza la lista en la vista*/
 function agregarAmigo() {
-  amigo = document.getElementById("amigo").value;
-  if (typeof amigo === "string" && amigo.length === 0) {
-    alert("Por favor, inserte un nombre.");
-  } else if (amigo === null) {
-    alert("Por favor, inserte un nombre.");
-  } else {
-    listadoAmigos.push(amigo);
-    limpiarCajaPorId();
-  }
+    amigo = document.getElementById("amigo").value;
+    if (typeof amigo === "string" && amigo.length === 0) {
+        alert("Por favor, inserte un nombre.");
+    } else if (amigo === null) {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        listadoAmigos.push(amigo);
+        limpiarCajaPorId();
+        actualizarLista(listadoAmigos);
+    }
 }
 
 function limpiarCajaPorId() {
-  document.getElementById("amigo").value = "";
+    document.getElementById("amigo").value = "";
+}
+
+// Funcion para actualizar la lista en la vista
+/* Parametros:
+    - listadoAmigos es la lista donde estan almacenados los amigos
+   Funcionamiento:
+    - Obtiene el elemento de la vista que contiene la lista
+    - Limpia la lista en la vista
+    - Se crean tantos elementos hayan sido guardados en la lista, y se muestran en la vista */
+function actualizarLista(listadoAmigos) {
+    lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    listadoAmigos.forEach(element => {
+        let li = document.createElement("li");
+        li.innerHTML = element;
+        lista.appendChild(li);
+    });
 }
